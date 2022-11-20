48dda51c05184cbe6cb4a680c0177dcfde75ccd9
Yujia Shi <yujiashishirley@gmail.com>
2022-11-20 16:42:34 -0500

    only leaving linking value when selecting

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import bubble from "./bubble.js";
 
+
 Promise.all([ // load multiple files
 	d3.csv('factorType.csv', d3.autoType)
 ]).then(data=>{
     const type = data[0];
     const bubbleChart = bubble(type, ".bubble");
-    var selectedFactor; // user selected factor
+    var selectedFactor, selectedCountry; // user selected factor and country
     d3.selectAll(".types").on("click", function (e, d) {bubbleChart.showFactor(d)
                                                         d3.selectAll(".factors").on("click", function (event, d) {selectedFactor = d.factorName;
                                                                                                                   console.log("selectedFactor", selectedFactor)
-                                                                                                                  document.getElementById("selected").textContent = "You have selected " + selectedFactor + ". Scroll down to embark the journey";});
-                                                    })
+                                                                                                                  document.getElementById("selected").textContent = "You have selected " + selectedFactor + ". Scroll down to embark the journey";;
+                                                                                                                  document.querySelector("[class$=ss-main]").style.display = "block";
+                                                                                                                  document.querySelector("[class$=ss-main]").style.width = "400px";
+                                                                                                                  document.querySelector("[class$=ss-main]").style.margin = "auto";
+                                                                                                                  selectedCountry = document.querySelector('.placeholder').innerHTML;
+                                                                                                                  console.log("selectedCountry", selectedCountry);
+                                                                                                                })
+                                                                                                                });
 });
