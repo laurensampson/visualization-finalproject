84388567565e7d907065a5e711162e64ee73e6c2
Yujia Shi <yujiashishirley@gmail.com>
2022-11-20 01:22:16 -0500

    Figuring out everything except for exporting

--- bubble.js
+++ bubble.js
@@ -1,6 +1,8 @@
 export default bubble;
 
+var returnFactor;
 function bubble(data, container){
+    var selectedType;
     // initialization
     const margin = ({top: 30, right: 50, bottom: 75, left: 50});
     const width = 1400 - margin.left - margin.right,
@@ -29,7 +31,17 @@ function bubble(data, container){
                                         .attr("opacity", 0.75)})
          .on("mouseout", function() {d3.select(this)
                                        .attr("opacity", 1)})
-         .on("click", (e, d) => showFactor(d))
+         .on("click", function (e, d) {svg.selectAll(".select-factor")
+                                         .data(d)
+                                         .enter()
+                                         .append("text")
+                                         .attr("class", "select-factor")
+                                         .attr("dx", 10)
+                                         .attr("dy", 225)
+                                         .attr("fill", "#606c76")
+                                         .text("Select a specific death factor to explore:");
+                                         selectedType = showFactor(d)
+                                        console.log("showFactor result saved", selectedType)})
          .merge(types)
          .transition()
          .delay(1000)
@@ -37,11 +49,8 @@ function bubble(data, container){
          .attr("cy", 75)
          .attr("r", 100)
          .attr("fill", d => colors(d));
-    var wrap = d3.textwrap()
-                 .bounds({height: 50, width: 50});
     var labels = svg.selectAll("text")
          .data(factorType)
-         .call(wrap)
     labels.enter()
           .append("text")
           .attr("class", "labels")
@@ -62,57 +71,63 @@ function bubble(data, container){
         console.log(type);
         var selectedFactors = data.filter(d => d.factorType == type);
         console.log(selectedFactors);
-        //let adjustment = 9 - selectedFactors.length;
-        //console.log("lower range bound", 100 + width / adjustment);
         xScale.domain(selectedFactors.map(d => d.factorName))
-              //.rangeRound([100 + width / adjustment, 100 + width * (adjustment - 1) / adjustment]);
               .rangeRound([75 + width / 2 - selectedFactors.length * 90, 75 + width / 2 + selectedFactors.length * 90])
         var factors = svg.selectAll(".factors")
                          .data(selectedFactors)
-        console.log("selectedFactor", xScale(selectedFactors.factorName))
         factors.enter()
                .append("circle")
                .attr("class", "factors")
                .attr("cx", width / 2)
-               .attr("cy", 300)
+               .attr("cy", 350)
                .attr("r", 90)
                .attr("fill", d => colors(d.factorType))
                .on("mouseover", function() {d3.select(this)
                                               .attr("opacity", 0.75)})
                .on("mouseout", function() {d3.select(this)
                                              .attr("opacity", 1)})
-               .on("click", (e, d) => console.log(d.factorName))
+               .on("click", function (e, d) {console.log("clicked on", d.factorName)
+                returnFactor = d.factorName})
                .merge(factors)
                .transition()
                .delay(100)
                .attr("cx", d => xScale(d.factorName))
-               .attr("cy", 300)
+               .attr("cy", 350)
                .attr("r", 90)
                .attr("fill", d => colors(d.factorType));
         factors.exit()
                .remove();
-        
         var names = svg.selectAll(".names")
                        .data(selectedFactors)
         names.enter()
-             .append("text")
+             .append("foreignObject")
              .attr("class", "names")
-             .attr("text-anchor", "middle")
-             .attr("fill-opacity", 0)
-             .attr("dx", d => xScale(d.factorName))
-             .attr("dy", 310)
-             .style("font-size", "8px")
+             .attr("text-anchor", "center")
+             .attr("x", d => xScale(d.factorName) - 80)
+             .attr("y", d => 335 - d.factorName.length * 0.25)
+             .attr("width", 160)
+             .attr("height", 160)
+             .append("xhtml:div")
+             .append("p")
+             .text(d => d.factorName)
+             .style("font-size", "15px")
              .merge(names)
              .transition()
              .delay(100)
              .attr("fill-opacity", 1)
-             .attr("dx", d => xScale(d.factorName))
-             .attr("dy", 310)
+             .attr("x", d => xScale(d.factorName) - 80)
+             .attr("y", d => 335 - d.factorName.length * 0.25)
+             .attr("width", 160)
+             .attr("height", 160)
+             .attr("stroke", "black")
              .text(d => d.factorName)
              .style("font-weight", "bold")
-             //.style("margin", "2px")
-             //.style("font-size", "10px");
+             .style("font-size", "15px")
+             .style("color", "black")
         names.exit()
              .remove();
+        //console.log("returnFactor", returnFactor)
+        return returnFactor;
     }
+    return selectedType;
 }
