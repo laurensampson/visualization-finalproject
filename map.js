7ba50f8f9b50ca9281c25b27400c94f4c5680045
Yujia Shi <yujiashishirley@gmail.com>
2022-12-03 18:26:57 -0500

    Finishing the map and barchart binding

--- map.js
+++ map.js
@@ -4,15 +4,22 @@ function map(factor){
     const spec = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
         "description": "A world map showing the death factor in color gradient corresponding to each country on the map",
-        "width": 700,
-        "height": 300,
-          "data": {"url": "map.csv"},
-          "params":[{"name": "year", 
-                   "value": 1990,
-                   "bind": {"input": "range",
-                            "min": 1990,
-                            "max": 2019,
-                            "step": 1}}],
+        //"width": 1400,
+        //"height": 500,
+        "data": {"url": "map.csv"},
+        "params":[{"name": "year", 
+                  "value": 1990,
+                  "bind": {"input": "range",
+                          "min": 1990,
+                          "max": 2019,
+                          "step": 1}},
+                  {
+                    "name": "invalidCode",
+                    "value": "M"
+                  }],
+        "vconcat": [{
+          "width": 800,
+          "height": 400,
           "transform": [{
             "lookup": "id",
             "from": {
@@ -26,15 +33,45 @@ function map(factor){
             "key": "id",
             "fields":["properties", "type", "geometry"]
             }},
-          {"filter": "datum.Year==year"}
-        ],
-        "projection": { "type":"equirectangular"},
-        "mark": "geoshape", 
-        "encoding":{"color":{"field": `${factor}`, "type": "quantitative"},
-                    "tooltip": [
-                      {"type": "nominal", "field": "Code"},
-                      {"field": `${factor}`, "type": "quantitative"}
-      ]},
-    }
+          {"filter": "datum.Year==year"},
+          ],
+          "projection": { "type":"equirectangular"},
+          "mark": "geoshape", 
+          "encoding":{"color":{"field": `${factor}`, "type": "quantitative"},
+                      "tooltip": [
+                        {"type": "nominal", "field": "Code"},
+                        {"field": `${factor}`, "type": "quantitative"}]}},
+          {
+            "width": 800,
+            "height": 200,
+            "transform": [
+              {"filter": "datum.Year==year"},
+              {"filter": "datum.id!=0"},
+              {"calculate": `datum['${factor}'] * 1`, "as": "Deaths"},
+              {
+                "window": [{
+                  "op": "count",
+                  "as": "count"
+                }],
+                "sort": [{"type": "quantitative", "field": `Deaths`, "order": "descending" }]
+              },
+              {"filter": "datum.count <=10"}
+            ],
+            "mark": "bar",
+            "encoding": {
+              "x": {
+                  "field": "Code",
+                  "type": "nominal",
+                  "title": "Country",
+                  "sort": {"field": `${factor}`, "order":"descending"}
+              },
+              "y": {
+                  "field": `Deaths`,
+                  "type": "quantitative"
+                  //"sort": null
+              }
+          }}]}
+
+          
     return spec;
 }
